122aada1f12fdc11544d1a435009467b522d7494
AlexSND <snd.dnb@gmail.com>
2018-06-04 01:37:44 +0800

    favico

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchQuery: 'irk',
+      searchQuery: 'london',
       locationName: '',
       currentTemp: '',
       icon: '',
